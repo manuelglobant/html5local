5e27f415965fa835efe000742924e939f86bfefd
Manuel Aberg Cobo <manuel.aberg@globant.com>
2015-06-08 15:58:17 -0300

    add network to manifest

--- app-pouch.js
+++ app-pouch.js
@@ -1,12 +1,12 @@
 /* global PouchDB*/
 (function () {
   'use strict';
-  var dblocal, remotedb, _id, posts, text, postsUl;
+  var dblocal, remotedb, text, postsUl;
 
   text = document.getElementById('post-form-text');
   postsUl = document.getElementById('post-list');
   dblocal = new PouchDB('posts');
-  remotedb = new PouchDB('http://localhost:5984/posts');
+  remotedb = new PouchDB('http://localhost:5984/posts', { ajax: { timeout: 10000 } });
 
   text.onkeypress = function (e) {
     if (!e) e = window.event;
@@ -19,23 +19,11 @@
 
   function savePost (post) {
     var newPost = {
-      '_id': _id.toString(),
+      '_id': new Date().toISOString(),
       'name': post.toString()
     };
     dblocal.put(newPost);
-    getPosts();
     dblocal.replicate.to(remotedb);
   }
-
-  function getPosts () {
-    dblocal.allDocs({
-      include_docs: true
-    }).then(function (result) {
-      _id = result.rows.length;
-      posts = result;
-    });
-  }
-
-  getPosts();
 }());
 
